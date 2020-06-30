2fe1203bab6bd8635206a1afeb9e1a01844323a1
x3mmf <vedafv@gmail.com>
2020-06-30 13:56:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+a = 5;
+console.log(a);
+
+var leftBorderWidth = 1;
+console.log(leftBorderWidth);
+var number = 5;
+var string = "hello!";
+var sym = Symbol();
+let second = 2;
+
+const pi = 3.14;
+
+let person = {
+  name: "jhon",
+  age: 17,
+  isMarried: false,
+};
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+// console.log(arr[2]);
+
+// alert("Hello world!");
+//let answer = +prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof answer);
+
+//console.log(4 + " - object");
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5 % 2);
+console.log("2" === 2);
+let isChecked = true,
+  isClose = false;
+console.log(isChecked || !isClose);
